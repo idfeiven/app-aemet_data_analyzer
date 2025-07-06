9de81b5d32ecddadb46bcb303da378d64c11b337
ivan.dominguez <ivandominguezfuentes64@gmail.com>
2025-07-06 10:33:17 +0200

    [UPD] Updated page organization. Added homepage

--- app/app_aemet_data_analyzer.py
+++ app/app_aemet_data_analyzer.py
@@ -3,16 +3,16 @@ import sys
 import streamlit as st
 sys.path.append(os.path.join(os.path.dirname(__file__), "modules"))
 
-homepage = st.Page("modules/stations_location.py", title = "Inicio")
+homepage = st.Page("modules/homepage.py", title = "AEMET Data Analyzer")
+stations_location = st.Page("modules/stations_location.py", title = "Ubicación de las estaciones de AEMET")
 last_obs = st.Page("modules/last_observation.py", title = "Últimas observaciones")
 historical_data = st.Page("modules/historical_data.py", title = "Datos históricos")
 normal_values = st.Page("modules/normal_values.py", title = "Valores normales (1991-2020)")
 
-pg = st.navigation([homepage,
-                    last_obs,
-                    historical_data,
-                    normal_values
-                    ]
-                  )
+pages = {
+    "Inicio": [homepage],
+    "Observación": [stations_location, last_obs, historical_data, normal_values]}
+
+pg = st.navigation(pages)
 
 pg.run()
---
+++ app/modules/homepage.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title("Bienvenido a la app web AEMET Data Analyzer")
+st.write("Esta aplicación te permite explorar, analizar y descargar datos meteorológicos de AEMET.")
+st.write("Puedes navegar por las diferentes secciones utilizando el menú de navegación en la parte superior.")
+st.write("Para comenzar, selecciona una de las opciones del menú:")
+st.markdown("""
+- **Ubicación de las estaciones**: Muestra un mapa interactivo con la ubicación de las estaciones meteorológicas de AEMET.
+- **Últimas observaciones**: Consulta las últimas observaciones meteorológicas de las estaciones.
+- **Datos históricos**: Explora los datos históricos de las estaciones meteorológicas.
+- **Valores normales (1991-2020)**: Consulta los valores normales de las variables meteorológicas para el periodo 1991-2020.
+""")
+st.write("Para más información, visita la [página oficial de AEMET](https://www.aemet.es/).")
+st.write("Si encuentras útil esta aplicación, tienes dudas o sugerencias, puedes contactar conmigo a través de mis redes: [Bluesky](https://bsky.app/profile/idfeiven.bsky.social) o [LinkedIn](https://www.linkedin.com/in/iv%C3%A1n-dom%C3%ADnguez-fuentes-5578a22ab/)")
+
+st.write("¡Espero que disfrutes de la aplicación!")
+
+st.write("Toda la información aquí expuesta pertenece a AEMET y se utiliza con fines educativos y de análisis.")
